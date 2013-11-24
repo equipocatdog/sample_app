892e8080561b87322da9a7303d1ac92b7d2cb390
Manuel <descremado_@hotmail.com>
2013-11-24 15:11:47 -0700

    Capitulo 4 finalizado, ejercicios pendiendtes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)                                #definicion del metodo
+    base_title = "Ruby on Rails Tutorial Sample App"        #asignacion a variable
+    if page_title.empty?                                    #condicion "si es true (si está vacio)"
+      base_title                                            #return
+    else
+      "#{base_title} | #{page_title}"                       #return
+    end
+  end
+
 end
